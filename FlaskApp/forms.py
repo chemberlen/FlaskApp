a5374f61705c35904ab44d9e5fb3bde27ed9e07c
chemberlen <7379167@gmail.com>
2018-12-12 13:28:04 +0200

    Remote configured connection

--- FlaskApp/forms.py
+++ FlaskApp/forms.py
@@ -3,8 +3,9 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 from FlaskApp.models import User
-# from FlaskApp.models import User
 
+
+# from FlaskApp.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
